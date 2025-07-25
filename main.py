5a201e184f9151fd1ae0fc0ed1780b488a5436e8
DanishAddie <danish.addie@bnm.gov.my>
2025-07-25 15:47:48 +0800

    modularized function for readability

--- main.py
+++ main.py
@@ -4,11 +4,28 @@ from src import fileParser
 from src import internetSearch
 import os
 
+# Functions
+def ParsePDFAndSearch(pdfBytes, gemini_api_key, tavily_api_key):
+
+    # parse pdf
+    fileParserOutput = dict(fileParser.ParseFile(pdfBytes, gemini_api_key).output)
+            
+    # to access financialMetrics: = dict(fileParserOutput["financialMetrics"])
+    # to access pdfSummary: = fileParserOutput["pdfSummary"]
+    # to access analyst: = dict(fileParserOutput["analyst"])
+    ticker = fileParserOutput["ticker"]
+    period = fileParserOutput["period"]
+
+    # search
+    searchResult = internetSearch.Search(ticker, period, gemini_api_key, tavily_api_key).output
+    fileParserOutput["searchResult"] = searchResult
+
+    return fileParserOutput
+
 # API key
 load_dotenv()
 gemini_api_key = os.getenv("GEMINI_API_KEY")
 tavily_api_key = os.getenv("TAVILY_API_KEY")
-
 
 # Streamlit Interface
 prompt = st.chat_input(
@@ -20,19 +37,9 @@ if prompt:
     if prompt["files"]:
         pdfBytes = prompt["files"][0].read()
         
-        fileParserOutput = dict(fileParser.ParseFile(pdfBytes, gemini_api_key).output)
-            
-        financialMetrics = dict(fileParserOutput["financialMetrics"])
-        pdfSummary = fileParserOutput["pdfSummary"]
-        analyst = dict(fileParserOutput["analyst"])
-        ticker = fileParserOutput["ticker"]
-        period = fileParserOutput["period"]
-
-
-        searchResult = internetSearch.Search(ticker, period, gemini_api_key, tavily_api_key).output
-        fileParserOutput["searchResult"] = searchResult
-
-        st.write(fileParserOutput)
+        content = ParsePDFAndSearch(pdfBytes, gemini_api_key, tavily_api_key)
+        
+        st.write(content)
 
     else:
         st.write("PDF Required")
